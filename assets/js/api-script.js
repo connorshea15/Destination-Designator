de91b716f715b5863c6937e8c978cdf17b0f8f57
connorshea15 <connorshea15@gmail.com>
2020-07-09 14:30:31 -0700

    I am using the national parks api to get a list of parks associated with a certain activity

--- assets/js/api-script.js
+++ assets/js/api-script.js
@@ -1,1 +1,18 @@
 // This is my javascript File
+
+
+var getParkInfo = function() {
+    var apiUrl = "https://developer.nps.gov/api/v1/activities/parks?q=hunting&api_key=SL6NUAYKuQc9Oci5uYIesi64ujlohyYU5Oshn7zb";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+getParkInfo();
